81ec942f3b0e6d7cd91a005d3113aa42dc88abdd
afifnur321 <48319307+afifnur321@users.noreply.github.com>
2019-03-07 21:58:11 +0700

    Add files via upload

---
+++ Afif.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afifa;
+
+/**
+ *
+ * @author GL62M-7REX
+ */
+public class Afif {
+    public static void main(String[] args) {
+        System.out.println("Nama    : NUR AFIF FADHLULLOH");
+        System.out.println("Nim     : 17090054 ");
+        System.out.println("Kelas   : Teknik Informatika 4A");
+    }
+    
+}
